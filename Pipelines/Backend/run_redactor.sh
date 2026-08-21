ad7dce46cb929b273e8ab9f1939f32baacfe79e3
JosefineM1603 <josefine@familienchristensen.com>
2026-08-21 07:26:34 +0200

    Something (#47)
    * Frontend (#4)
    
    * Urgent fix of pyproject.toml + setup for frontend pipeline
    
    * installed gooey
    
    * Before messing with packaging my GUI for windows
    
    * Test
    
    ---------
    
    Co-authored-by: MatiasAppel <127248331+MatiasAppel@users.noreply.github.com>
    
    * Adding backend packages
    
    * Backend (#46)
    
    * First commit. Some patching of the script + OCR handling
    
    * Updated functionality for ocr and redaction
    
    * Newest functionality, split into modular setup with main/function scripts. Added readme file.
    
    * bash script for running redactor
    
    * Newest backend functionality and main
    
    * uv lock and toml file
    
    * gitignore
    
    * Updating all the things
    
    * Updated functionality for ocr and redaction
    
    * Newest functionality, split into modular setup with main/function scripts. Added readme file.
    
    * bash script for running redactor
    
    * Newest backend functionality and main
    
    * Updating all the things
    
    ---------
    
    Co-authored-by: MatiasAppel <127248331+MatiasAppel@users.noreply.github.com>

---
+++ Pipelines/Backend/run_redactor.sh
@@ -0,0 +1,8 @@
+cd '/work/MatiasKokholmAppel#3073/RP_ANON'
+
+uv run -m Pipelines.Backend.main_pdf_redactor \
+    --input "/work/RP_ANON/ANON/Data/unmarked/Underretning 04-12-2025_unmarked.pdf" \
+    --output-dir "/work/MatiasKokholmAppel#3073/RP_ANON/Pipelines/Backend/Data/output" \
+    --categories cpr case-id address \
+    --pattern Katrine \
+    --replacement "[REDACTED]"
